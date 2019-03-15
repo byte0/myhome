237c9883b581735729a6228b6c1950a39f0ca74a
code1031 <1031247267@qq.com>
2019-03-15 11:26:49 +0800

    完成聊天窗口的控制逻辑

--- src/module/chat/index.js
+++ src/module/chat/index.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import './index.css';
+import './chat.css';
 import axios from 'axios';
+import { Icon, Form, TextArea, Button } from 'semantic-ui-react';
 import {baseURL} from '../../common';
+
+class ChatWindow extends React.Component {
+  render() {
+    let {close} = this.props;
+    return (
+      <div className='chat-window'>
+        <div className="chat-window-title">
+          <Icon onClick={close} name='angle left' className='chat-ret-btn' size='large'/>
+          <span>名字</span>
+        </div>
+        <div className="chat-window-content">
+          <ul>
+            <li>列表信息</li>
+          </ul>
+        </div>
+        <div className="chat-window-input">
+          <Form>
+            <TextArea placeholder='请输入内容...' />
+            <Button>关闭</Button>
+            <Button primary>发送</Button>
+          </Form>
+        </div>
+      </div>
+    );
+  }
+}
 
 class Chat extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      listData: []
+      listData: [],
+      open: false    // 控制聊天窗口的显示和隐藏
     }
   }
   
@@ -19,10 +48,24 @@ class Chat extends React.Component {
     });
   }
 
+  toChat = () => {
+    // 显示聊天窗口
+    this.setState({
+      open: true     
+    });
+  }
+
+  // 关闭聊天窗口
+  closeWindow = () => {
+    this.setState({
+      open: false
+    });
+  }
+
   render() {
     let listInfo = this.state.listData.map(item=>{
       return (
-        <li key={item.id} onClick={(e) => this.toChat(e,{item})}>
+        <li key={item.id} onClick={(e) => this.toChat()}>
           <div className="avarter">
             <img src={baseURL + item.avatar} alt="avarter"/>
             <span className="name">{item.username}</span>
@@ -40,6 +83,7 @@ class Chat extends React.Component {
             {listInfo}
           </ul>
         </div>
+        {this.state.open && <ChatWindow close={this.closeWindow}/>}
       </div>
     );
   }
